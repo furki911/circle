c9dd5c66d3c6e3b6b9a6c8d99fb65bad8f312602
furki911 <55999845+furki911@users.noreply.github.com>
2020-06-29 00:37:18 +0530

    Version 1.0

---
+++ script.js
@@ -0,0 +1,47 @@
+var slide_index = 1;  
+
+displaySlides(slide_index);  
+function nextSlide(n) {  
+   displaySlides(slide_index += n);  
+}  
+   
+function currentSlide(n) {  
+   displaySlides(slide_index = n);  
+}  
+   
+function displaySlides(n) {  
+   var i;  
+   var slides = document.getElementsByClassName("showSlide");  
+   if (n > slides.length) { slide_index = 1 }  
+   if (n < 1) { slide_index = slides.length }  
+   for (i = 0; i < slides.length; i++) {  
+      slides[i].style.display = "none";  
+   }  
+   slides[slide_index - 1].style.display = "block";  
+} 
+
+var element = document.querySelector('.navbar__links'); 
+var btn = document.querySelector('.navbar__links--item--btn');
+
+var boolean = true;
+
+function clicked() {
+      var flag = window.matchMedia('(max-width: 37.5em)').matches;
+      if (flag) {
+         element.classList.toggle('display');
+         setAtt(boolean);
+      }
+};
+
+(clicked)();
+
+function setAtt(check) {
+
+   if (!boolean) {
+      btn.setAttribute('name', 'active');
+      boolean = true;
+   } else {
+      btn.setAttribute('name', 'inactive');
+      boolean = false;
+   }
+};
